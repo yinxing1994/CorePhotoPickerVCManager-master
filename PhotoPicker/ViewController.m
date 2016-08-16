05ea1aee04f52630806b083b83ab1b03ddebc2f1
yinxing1994 <yinxing@yinxingdeMacBook-Pro.local>
2016-08-16 20:58:24 +0800

    拍照demo
    封装简单易用

---
+++ PhotoPicker/ViewController.m
@@ -0,0 +1,84 @@
+//
+//  ViewController.m
+//  PhotoPicker
+//
+//  Created by muxi on 15/2/13.
+//  Copyright (c) 2015年 muxi. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CorePhotoPickerVCManager.h"
+
+
+@interface ViewController ()<UIActionSheetDelegate>
+
+@property (nonatomic,strong) CorePhotoPickerVCManager *manager;
+@property (strong, nonatomic) IBOutlet UIImageView *imageV;
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)btnClick:(id)sender {
+   
+    UIActionSheet *sheet=[[UIActionSheet alloc] initWithTitle:@"请选取" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"拍摄" otherButtonTitles:@"照片库",@"照片多选", nil];
+    
+    [sheet showInView:self.view];
+    
+    
+}
+
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    CorePhotoPickerVCMangerType type=0;
+
+    
+    if(buttonIndex==0) type=CorePhotoPickerVCMangerTypeCamera;
+    
+    if(buttonIndex==1) type=CorePhotoPickerVCMangerTypeSinglePhoto;
+    
+    if(buttonIndex==2) type=CorePhotoPickerVCMangerTypeMultiPhoto;
+
+    CorePhotoPickerVCManager *manager=[CorePhotoPickerVCManager sharedCorePhotoPickerVCManager];
+    
+    //设置类型
+    manager.pickerVCManagerType=type;
+    
+    //最多可选5张
+    manager.maxSelectedPhotoNumber=5;
+    
+    //错误处理
+    if(manager.unavailableType!=CorePhotoPickerUnavailableTypeNone){
+        NSLog(@"设备不可用");
+        return;
+    }
+    
+    UIViewController *pickerVC=manager.imagePickerController;
+    
+    //选取结束
+    manager.finishPickingMedia=^(NSArray *medias){
+        
+        [medias enumerateObjectsUsingBlock:^(CorePhoto *photo, NSUInteger idx, BOOL *stop) {
+            NSLog(@"%@",photo.editedImage);
+            _imageV.image = photo.editedImage;
+        }];
+        
+        
+    };
+    
+    [self presentViewController:pickerVC animated:YES completion:nil];
+
+    
+}
+
+
+
+@end
